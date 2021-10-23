b3b3aa2473dfef1d6d9b2220a5f19d62a4382005
Noah Slyker <noahslyker@gmail.com>
2021-10-22 22:09:46 -0500

    got all data added and also got data to show on pages

---
+++ db/migrate/20211023012022_create_pokemen.rb
@@ -0,0 +1,10 @@
+class CreatePokemen < ActiveRecord::Migration[6.1]
+  def change
+    create_table :pokemen do |t|
+      t.string :name
+      t.string :url
+
+      t.timestamps
+    end
+  end
+end
